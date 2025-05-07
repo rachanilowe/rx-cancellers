3e781de03047f7118edff337277ceac840a3b077
Ted Kim <tedkim7@berkeley.edu>
2025-05-06 17:16:29 -0700

    Design passing synthesis

--- src/main/scala/firSegment.scala
+++ src/main/scala/firSegment.scala
@@ -21,25 +21,8 @@ class FIRSegment(val segmentSize: Int, val gammaFactor: Int, val muFactor: Int) 
   val weights = RegInit(VecInit(Seq.fill(segmentSize)(0.S(8.W))))
   when (io.valid) {
     for (i <- 0 until segmentSize) {
-      // val deltaW = (io.weightCalcIns(i) * (io.error))
-      // val errorW = io.error.pad(14) 
-
-      // val deltaW = MuxLookup(io.weightCalcIns(i).asUInt, 0.S, Seq(
-      //   0.U -> 0.S,                                //  0
-      //   1.U -> errorW,                            //  1
-      //   2.U -> (errorW << 1),                   //  2
-      //   3.U -> (errorW + (errorW << 1)),     //  3
-      //   4.U -> -(errorW << 2),                 // -4
-      //   5.U -> -(errorW + (errorW << 1)),   // -3
-      //   6.U -> -(errorW << 1),               // -2
-      //   7.U -> -errorW                       // -1
-      // ))
-
       // Removed 1.max
       weights(i) := (((((1 << gammaFactor) - 1)).asSInt * weights(i)) >> gammaFactor) + (((io.weightCalcIns(i) * (io.error)) >> muFactor))
-      // weights(i) := ((weights(i)) >> gammaFactor) + (((io.weightCalcIns(i) * (io.error)) >> muFactor))
-      // weights(i) := ((weights(i)) >> gammaFactor) + ((deltaW) >> muFactor)
-      // weights(i) := weightUpdate
     }
   }
 
